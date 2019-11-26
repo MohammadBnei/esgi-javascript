baaccc1fded34f668d313d7d74fcf5b36a7c7e92
MohammadBnei <mohammadamine.banaei@gmail.com>
2019-11-26 17:07:06 +0100

    [DONE] exercice-1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -24,6 +24,7 @@ function capitalize(str) {
 		.join(" ");
 }
 
+console.log("Capitalize");
 console.log(capitalize("test"));
 console.log(capitalize("Test"));
 console.log(capitalize(" test"));
@@ -34,15 +35,30 @@ console.log(capitalize({}));
 
 const camelCase = str => {
 	if (verifyStringType(str)) return "";
-	return capitalize(str)
-		.split(" ")
-		.join("");
+	return capitalize(str).replace(/[\s+_!@#$%^&*(),.?":{}|<>]/g, "");
 };
 
+console.log("Camel Case");
 console.log(camelCase("test"));
 console.log(camelCase("Test"));
 console.log(camelCase(" test"));
-console.log(camelCase("test TEst tst"));
+console.log(camelCase("test TE2st_tst"));
 console.log(camelCase(""));
 console.log(camelCase(null));
 console.log(camelCase({}));
+
+const snake_case = str => {
+	if (verifyStringType(str)) return "";
+
+	return str.toLowerCase().replace(/\s/g, "_");
+};
+
+console.log("Snake Case");
+
+console.log(snake_case("test"));
+console.log(snake_case("Test"));
+console.log(snake_case(" test"));
+console.log(snake_case("test TEst tst"));
+console.log(snake_case(""));
+console.log(snake_case(null));
+console.log(snake_case({}));
